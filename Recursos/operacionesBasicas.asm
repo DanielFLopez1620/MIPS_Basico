70cc2dbdacecd06a848df3fd414114d065e19ae7
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2021-10-20 10:43:57 -0500

    Definicion_Variables_operacionesBasicas

--- Recursos/operacionesBasicas.asm
+++ Recursos/operacionesBasicas.asm
@@ -1,3 +1,12 @@
-.data
-
+.data   #Apartado de declaración de datos
+    num1: .word 15  #.word para representar datos enteros
+	num2: .word 10  
+	letrero1: .asciiz "Suma: "  #.ascciz para representar cadenas o string
+	letrero2: .asciiz "Resta: "
+    letrero3: .asciiz "Multiplicacion: "
+    letrero4: .asciiz "Division: "
+	saltoLinea: .asciiz "\n" # \n representa un enter o salto de línea
 .text
+    main: # Etiqueta de función base o principal
+        li $v0, 10  # Preparación para finalizar ejecución
+        syscall 
