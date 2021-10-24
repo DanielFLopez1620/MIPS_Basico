d0409b408cc5b4b22a6201ffcdac4e993d911eb4
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2021-10-24 09:16:08 -0500

    arreglosRecursos

--- Recursos/arreglosBasico.asm
+++ Recursos/arreglosBasico.asm
@@ -1,26 +1,24 @@
 .data
-    tabla_numeros:   .byte  7, 4
+    tabla_numeros:   .byte  1, 2, 3, 4, 5
     hola: .asciiz "Hola"
 .text
-.globl main
-main: 
-    la $k0, tabla_numeros
-    lb $t0, 0($k0)
-    lb $t1, 1($k0)
-    li $v0, 1
-    add $a0, $zero , $t0 
-    syscall
-    li $v0, 1
-    add $a0, $zero , $t1 
-    syscall
-    bne $t0, $t1, imprimir
-    syscall
-    # --Preparación para finalizar ejecución-----
-    li $v0, 10  
-    syscall 
-imprimir:
-    li $v0, 4
-    la $a0, hola
-    syscall
-
-    
+    .globl main
+    main: 
+        la $k0, tabla_numeros
+        lb $t0, 0($k0)
+        lb $t1, 1($k0)
+        li $v0, 1
+        add $a0, $zero , $t0 
+        syscall
+        li $v0, 1
+        add $a0, $zero , $t1 
+        syscall
+        bne $t0, $t1, imprimir
+        syscall
+        # --Preparación para finalizar ejecución-----
+        li $v0, 10  
+        syscall 
+    imprimir:
+        li $v0, 4
+        la $a0, hola
+        syscall
